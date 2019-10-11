3569f41a7e93a04315e1784faf055ac07aaa7726
Mateusz Czapliński <czapkofan@gmail.com>
2019-10-11 22:45:42 +0200

    add CLI flags parsing

--- src/apksign.nim
+++ src/apksign.nim
@@ -7,6 +7,7 @@ import strutils
 import tables
 import nimcrypto
 import os, osproc
+import parseopt
 
 when defined windows:
   {.passl: "-lz".}
@@ -17,6 +18,8 @@ const
   signKey = "test.key.pem"
   signCert = "test.cert.pem"
   tmpFile = "tmp"
+  usage = """
+USAGE: apksign -i=DIRECTORY -o=FILE.apk -c=cert.pem -k=key.pem"""
 
 type
   Signer = object
@@ -29,6 +32,41 @@ type
 main()
 
 proc main() =
+  # Parse flags
+  var
+    input: string
+    output: string
+    cert: string
+    key: string
+  for kind, flag, val in getopt():
+    case flag
+    of "i":
+      if val == "": die "flag `-i` requires a directory parameter, e.g.: -i=apk/"
+      if not dirExists val: die "flag `-i` requires a directory as a parameter, got: " & val
+      input = val
+    of "o":
+      if val == "": die "flag `-o` requires a filename parameter, e.g.: -o=myapp.apk"
+      output = val
+    of "c":
+      if val == "": die "flag `-c` requires a filename parameter, e.g.: -c=cert.pem"
+      cert = val
+    of "k":
+      if val == "": die "flag `-k` requires a filename parameter, e.g.: -k=key.pem"
+      key = val
+    else:
+      die "unknown flag: " & $kind & flag & "\n" & usage
+  if input == "":  die "missing `-i` flag\n" & usage
+  if output == "": die "missing `-o` flag\n" & usage
+  if cert == "":   die "missing `-c` flag\n" & usage
+  if key == "":    die "missing `-k` flag\n" & usage
+  if fileExists(input & "/META-INF/MANIFEST.MF"):
+    die "modifying existing META-INF/MANIFEST.MF file not yet implemented"
+
+  # TODO: take files from apk/ directory (flag -i)
+  # TODO: calculate manifest.mf, cert.sf, cert.rsa
+  # TODO: add manifest & cert.{sf,rsa} to .zip file (flag -o)
+  # TODO: add remaining files to .zip file
+
   var src: ZipArchive
   doAssert src.open(apkIn, fmRead)
 
@@ -120,6 +158,16 @@ proc buildSignedApk(signer: var Signer, src: var ZipArchive) =
 
   dst.close()
 
+
+proc `$`(kind: CmdLineKind): string =
+  case kind
+  of cmdLongOption: "--"
+  of cmdShortOption: "-"
+  of cmdArgument, cmdEnd: ""
+
+proc die(s: string) =
+  stderr.write("apksign: " & s & "\n")
+  quit(1)
 
 proc skipFile(s: string): bool =
   let (dir, name, ext) = s.splitFile
